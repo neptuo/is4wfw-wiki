8f02ee8fa0fb4c5d438c9e36c328546b5fd5b035
Marek Fišera <mara@neptuo.com>
2021-10-08 10:50:05 +0200

    #1 - Search.

--- src/views/pages/home.view.php
+++ src/views/pages/home.view.php
@@ -1,4 +1,30 @@
-<ce:list name="page" orderBy-title="asc">
+<filter:declare name="page" alias="p">
+    <filter:or>
+        <filter:like name="content" contains="query:q" />
+        <filter:like name="title" contains="query:q" />
+    </filter:or>
+</filter:declare>
+
+<edit:form submit="search">
+    <ui:filter>
+        <bs:formGroup field="q">
+            <div class="input-group mb-3">
+                <ui:textbox name="q" class="bs:fieldValidatorCssClass" placeholder="Search..." />
+                <div class="input-group-append">
+                    <bs:button name="search">
+                        <fa5:icon name="search" />
+                        <span class="d-none d-md-inline">
+                            Search
+                        </span>
+                    </bs:button>
+                    <web:a pageId="route:home" class="btn btn-secondary">Clear</web:a>
+                </div>
+            </div>
+        </bs:formGroup>
+    </ui:filter>
+</edit:form>
+
+<ce:list name="page" filter="filter:page" orderBy-title="asc">
     <ui:any items="ce:list">
         <div class="list-group mb-4">
             <ce:register name="url" />
