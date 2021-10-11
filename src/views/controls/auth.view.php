94de5e3df5c93c0a7f24ccff685cab8ee74f2a44
Marek Fišera <mara@neptuo.com>
2021-10-11 21:00:28 +0200

    #1 - Last page before login page.

--- src/views/controls/auth.view.php
+++ src/views/controls/auth.view.php
@@ -1,4 +1,8 @@
-<login:refresh group="wiki" />
+<login:refresh group="wiki">
+    <web:condition when="web:currentPath" is="/login" isInverted="true">
+        <var:declare name="lastPage" value="web:currentPath" scope="session" />
+    </web:condition>
+</login:refresh>
 <web:condition when="post:logout" is="logout">
     <login:logout group="wiki">
         <var:declare name="lastPage" value="web:currentPath" scope="session" />
