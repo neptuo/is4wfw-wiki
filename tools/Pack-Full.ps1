9b1b04adf72f5a0402b1eddf9f1e6b00aa9d27ed
Marek Fišera <mara@neptuo.com>
2021-10-15 12:45:27 +0200

    #3 - Script for packing a release zip.

---
+++ tools/Pack-Full.ps1
@@ -0,0 +1,48 @@
+# Example: .\tools\Pack-Patch.ps1 v338.7
+param($versionName)
+
+# VersionName is required.
+if ($null -eq $versionName) 
+{
+    Write-Output "Missing required 'versionName' parameter.";
+    return;
+}
+
+Push-Location $PSScriptRoot;
+
+# Install composer.
+Invoke-Expression ".\Install-Composer.ps1";
+
+# Update version file.
+Invoke-Expression ".\Update-Version.ps1 $versionName";
+
+# Compute release name.
+$srcPath = "src";
+$artifactsPath = "artifacts";
+$releaseName = "is4wfw-wiki-" + $versionName;
+$targetFileName = $releaseName + ".zip";
+
+# Create artifacts directory.
+$targetFilePath = Join-Path -Path (Join-Path -Path (Get-Location) -ChildPath "..") -ChildPath $artifactsPath;
+if (!(Test-Path $targetFilePath))
+{
+    New-Item -ItemType Directory $targetFilePath | Out-Null;
+}
+    
+# Delete full file if exists.
+$targetFilePath = Join-Path -Path $targetFilePath -ChildPath $targetFileName;
+if (Test-Path($targetFilePath)) 
+{
+    Remove-Item $targetFilePath;
+}
+
+# Create new archive.
+$currentPath = $PSScriptRoot;
+$archiverPath = Join-Path -Path $currentPath -ChildPath "7za.exe";
+$sourceDirectoryPath = Join-Path -Path ".." -ChildPath $srcPath;
+Push-Location $sourceDirectoryPath;
+Invoke-Expression ($archiverPath + " a -tzip " + $targetFilePath) | Out-Null;
+Pop-Location;
+
+Write-Host "Created file '$targetFilePath'";
+Pop-Location;
