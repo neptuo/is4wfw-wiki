39da7615e893c889d451c61e63d2c7c3ef81127d
Marek Fišera <mara@neptuo.com>
2021-10-20 10:15:44 +0200

    #16 - Migrate to DbMigrator.

--- src/postupdate.inc.php
+++ src/postupdate.inc.php
@@ -1,9 +1,10 @@
 <?php
 
-require_once(APP_SCRIPTS_PHP_PATH . "classes/Module.class.php");
-require_once(APP_SCRIPTS_PHP_PATH . "classes/utils/StringUtils.class.php");
+    require_once(APP_SCRIPTS_PHP_PATH . "classes/Module.class.php");
+    require_once(APP_SCRIPTS_PHP_PATH . "classes/utils/FileUtils.class.php");
+    require_once(APP_SCRIPTS_PHP_PATH . "classes/dataaccess/DbMigrator.class.php");
 
-$module = Module::getById("9cce2ed5-3632-4e34-b1eb-a41e1200608a");
-require_once($module->getRootPath() . "autoupdate.inc.php");
+    $module = Module::getById("9cce2ed5-3632-4e34-b1eb-a41e1200608a");
+    DbMigrator::run(FileUtils::combinePath($module->getRootPath(), "data/autoupdate.xml"), "wiki-db_version");
 
 ?>
