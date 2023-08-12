bee011786ac3a0edd2dc942103453b2f69011cf6
Marek Fišera <mara@neptuo.com>
2023-08-12 23:04:08 +0200

    #40 - Empty message for no files

--- src/views/pages/files.view.php
+++ src/views/pages/files.view.php
@@ -1,42 +1,52 @@
 <controls:pageDetail url="template:url" includeDirectoryId="true">
     <login:authorized any="wiki">
-        <div>
-            <fa:browser dirId="cepage:directory_id">
-                <ui:grid items="fa:browserList" class="table table-hover" thead-class="bg-dark text-light">
-                    <ui:column header="Id" value="fa:browserId" />
-                    <ui:columnTemplate header="Name">
-                        <utils:concat output="var:fileName" separator="." value1="fa:browserName" value2="fa:browserExtension" />
-                        <web:out text="var:fileName" />
-                    </ui:columnTemplate>
-                    <ui:columnDateTime header="Modified" value="fa:timestamp" format="d.m.Y H:i:s" />
-                    <ui:columnTemplate header="Link">
-                        &#126;/file.php?rid=<web:out text="fa:browserId" />
-                    </ui:columnTemplate>
-                    <ui:columnTemplate>
-                        <a href="~/file.php?rid=<web:out text="fa:browserId" />" target="_blank">
-                            Open...
-                        </a>
-                    </ui:columnTemplate>
-                </ui:grid>
-            </fa:browser>
-        </div>
-        <div class="mt-4">
-            <edit:form submit="upload">
-                <web:out if:true="edit:saved">
-                    <web:redirectToSelf />
-                </web:out>
+        <web:out if:stringEmpty="cepage:directory_id" if:not="true">
+            <div>
+                <fa:browser dirId="cepage:directory_id">
+                    <ui:empty items="fa:browserList">
+                        <bs:alert color="warning">
+                            No attached files for this page
+                        </bs:alert>
+                    </ui:empty>
+                    <ui:grid items="fa:browserList" class="table table-hover" thead-class="bg-dark text-light">
+                        <ui:column header="Id" value="fa:browserId" />
+                        <ui:columnTemplate header="Name">
+                            <utils:concat output="var:fileName" separator="." value1="fa:browserName" value2="fa:browserExtension" />
+                            <web:out text="var:fileName" />
+                        </ui:columnTemplate>
+                        <ui:columnDateTime header="Modified" value="fa:timestamp" format="d.m.Y H:i:s" />
+                        <ui:columnTemplate header="Link">
+                            &#126;/file.php?rid=<web:out text="fa:browserId" />
+                        </ui:columnTemplate>
+                        <ui:columnTemplate>
+                            <a href="~/file.php?rid=<web:out text="fa:browserId" />" target="_blank">
+                                Open...
+                            </a>
+                        </ui:columnTemplate>
+                    </ui:grid>
+                </fa:browser>
+            </div>
+            <div class="mt-4">
+                <edit:form submit="upload">
+                    <web:out if:true="edit:saved">
+                        <web:redirectToSelf />
+                    </web:out>
 
-                <div style="display: none;">
-                    <fa:upload dirId="cepage:directory_id">
-                        <ui:filebox id="upload-box" name="files" isMulti="true" />
-                    </fa:upload>
-                    <button id="upload-submit" type="submit" name="upload" value="upload"></button>
-                </div>
-                <bs:button id="upload-button" color="primary" type="button">
-                    Upload...
-                </bs:button>
-            </edit:form>
-        </div>
+                    <div style="display: none;">
+                        <fa:upload dirId="cepage:directory_id">
+                            <ui:filebox id="upload-box" name="files" isMulti="true" />
+                        </fa:upload>
+                        <button id="upload-submit" type="submit" name="upload" value="upload"></button>
+                    </div>
+                    <bs:button id="upload-button" color="primary" type="button">
+                        Upload...
+                    </bs:button>
+                </edit:form>
+            </div>
+        </web:out>
+        <bs:alert color="warning" if:stringEmpty="cepage:directory_id">
+            Save the page before uploading files
+        </bs:alert>
     </login:authorized>
 </controls:pageDetail>
 
