e8a9f359ab1c9f76b03fea0bfcb1871bcbf06093
Marek Fišera <mara@neptuo.com>
2023-08-12 23:28:13 +0200

    #40 - Bottom/mobile menu

--- src/views/controls/template.view.php
+++ src/views/controls/template.view.php
@@ -18,6 +18,21 @@
             a.dropdown-item .fa, a.dropdown-item .fas {
                 width: 20px;
             }
+            
+            footer.navbar {
+                position: absolute;
+                bottom: 0;
+                width: 100%;
+            }
+
+            .navbar-bottom {
+                background: #f5f5f5;
+                border-top: 1px solid #ddd;
+            }
+
+            .navbar-bottom .h4 {
+                line-height: 1;
+            }
         </style>
     
         <web:out if:stringEmpty="var:wiki.icon.id" if:not="true">
@@ -30,7 +45,7 @@
     
     <controls:navbar />
 
-    <bs:container class="my-4">
+    <bs:container class="mt-4" style="margin-bottom: 72px">
         <var:use name="message" scope="temp" />
         <web:condition when="var:message">
             <bs:alert color="primary" class="mb-4">
@@ -44,4 +59,35 @@
         </web:condition>
         <router:render />
     </bs:container>
+
+    <template:declare identifier="bottomNavItem">
+        <template:attribute name="iconPrefix" default="fa" />
+
+        <bs:column>
+            <route:use name="template:route">
+                <var:declare name="cssClass" value="btn btn-block" />
+                <web:condition when="route:isActive">
+                    <var:declare name="cssClass" value="btn btn-block btn-primary" />
+                </web:condition>
+                <web:condition when="route:isActive" isInverted="true">
+                    <var:declare name="cssClass" value="btn btn-block btn-light text-dark" />
+                </web:condition>
+                <web:a pageId="route:url" class="var:cssClass">
+                    <fa5:icon prefix="template:iconPrefix" name="template:icon" />
+                </web:a>
+            </route:use>
+        </bs:column>
+    </template:declare>
+
+    <nav class="fixed-bottom d-md-none navbar-bottom py-2">
+        <bs:container>
+            <bs:row>
+                <template:bottomNavItem route="all" icon="file-alt" />
+                <template:bottomNavItem route="archive" icon="archive" />
+                <template:bottomNavItem route="favorites" icon="star" />
+                <template:bottomNavItem route="folderList" icon="folder" iconPrefix="fas" />
+            </bs:row>
+        </bs:container>
+    </nav>
+
 </web:condition>
