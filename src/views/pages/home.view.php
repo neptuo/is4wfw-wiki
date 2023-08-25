345fd5667b72f88df14aa663cbb38327afe2427a
Marek Fišera <mara@neptuo.com>
2023-08-25 15:12:43 +0200

    #4 - Show page with "home" url if exists on home.

--- src/views/pages/home.view.php
+++ src/views/pages/home.view.php
@@ -1,6 +1,19 @@
-<controls:pageListGrouped>
-    <filter:or>
-        <filter:null name="is_archived" />
-        <filter:equals name="is_archived" value="0" />
-    </filter:or>
-</controls:pageListGrouped>
+<filter:declare name="homePage" alias="p">
+    <filter:equals name="url" value="home" />
+    <login:authorized none="wiki">
+        <filter:equals name="is_public" value="1" />
+    </login:authorized>
+</filter:declare>
+<cepage:list filter="filter:homePage">
+    <ui:first items="cepage:list">
+        <pages:detail url="cepage:url" />
+    </ui:first>
+    <ui:empty items="cepage:list">
+        <controls:pageListGrouped>
+            <filter:or>
+                <filter:null name="is_archived" />
+                <filter:equals name="is_archived" value="0" />
+            </filter:or>
+        </controls:pageListGrouped>
+    </ui:empty>
+</cepage:list>
